efe93e124cb72d800fb77f4b21f13f4608ab32ac
junior <crce.8260.elec@gmail.com>
2026-05-21 20:52:41 +0530

    u

--- api/index.js
+++ api/index.js
@@ -18,9 +18,11 @@ app.get("/api", (req, res) => {
 });
 
 // local dev only
-if (process.env.NODE_ENV !== "production") {
-  const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-}
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("SERVER LIVE ON:", PORT);
+});
+
 
 export default app;
