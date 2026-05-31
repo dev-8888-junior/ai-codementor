c41ff70abb439208fca73cdbe38a80e40dcd19ff
junior <crce.8260.elec@gmail.com>
2026-06-01 00:49:52 +0530

    Fix Vercel config through api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API_URL =
-  import.meta.env.VITE_APP_API_URL;
+  import.meta.env.VITE_APP_API_URL ||
+  "http://localhost:5000/api";
 
 const API = axios.create({
   baseURL: API_URL,
